3e150d202f936b4d895cbf94f6319ce100c7613f
haboty1 <haboty@post.bgu.ac.il>
2022-12-15 17:19:24 +0200

    p

---
+++ static/script.js
@@ -0,0 +1,17 @@
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('li a').forEach(link => {    
+    if(link.href.includes(`${activePage}`)){
+      link.classList.add('active');
+    }
+  });
+
+  function validateEmail(email) {
+    const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return res.test(String(email).toLowerCase());
+    
+  }
+
+  if(pw.length < 8) {  
+    document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+    return false;  
+ }
